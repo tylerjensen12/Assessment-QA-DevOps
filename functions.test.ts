01968feb07caaf11a4fbcbe2254dc36aae52de85
Tyler Jensen <troubletimestwo97@gmail.com>
2022-11-04 11:17:16 -0600

    Unit test completed

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,13 @@
 const {shuffleArray} = require('./utils')
+const {bots} = require('./data')
 
 describe('shuffleArray should', () => {
     // CODE HERE
+    test('if its the same length', () => {
+        expect(shuffleArray(bots).length).toEqual(shuffleArray(bots).length)
+    })
+
+    test('if it returns an array shuffled', () => {
+        expect(shuffleArray(bots)).not.toEqual(shuffleArray(bots))
+    })
 })
